aa51ecbe36e2781afdc1249e79701bf4607ce6b7
ferdelaro <131069557+ferdelaro@users.noreply.github.com>
2023-06-12 20:26:55 -0300

    Uso de API y mejoras en formulario

--- js/index.js
+++ js/index.js
@@ -9,7 +9,6 @@ function verificar() {
     var email = document.getElementById("email").value;
     // Verificar si el correo electrónico es válido
     if (validarEmail(email)) {
-        alert("El correo electrónico es válido");
     } else {
         alert("El correo electrónico es inválido");
     }
@@ -24,3 +23,34 @@ function verificar() {
 		return false;
 	}
 }
+function enviarFormulario() {   
+    // Mostrar el mensaje de enviado después de enviar el formulario
+    document.getElementById('formulario').style.display = 'none';
+    document.getElementById('mensaje-enviado').style.display = 'flex';
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = 'CA7JZphjiTw3U9ciIQrxI1IykxrNfZ3SLO7bLglaDhU';
+    const imageContainer = document.getElementById('image-container');
+  
+    function fetchPexelsImage() {
+      fetch('https://api.pexels.com/v1/search?query=nature', {
+        headers: {
+          Authorization: apiKey
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          const image = data.photos[0];
+          const img = document.createElement('img');
+          img.src = image.src.medium;
+          img.alt = image.photographer;
+          imageContainer.appendChild(img);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  
+    fetchPexelsImage();
+  });
